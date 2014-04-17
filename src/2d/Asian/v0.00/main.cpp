feede44725ecf24044e09342c72ab74ee75e67e6
baba12345 <giorgio.re27@gmail.com>
2014-04-17 11:16:41 +0200

    asian_dontknowifworks

--- src/2d/Asian/v0.00/main.cpp
+++ src/2d/Asian/v0.00/main.cpp
@@ -50,6 +50,8 @@
 #include <gsl/gsl_interp.h>
 #include <gsl/gsl_spline.h>
 
+
+
 using namespace std;
 using namespace dealii;
 
@@ -57,19 +59,16 @@ const double toll=1e-8;
 
 // max(s1+s2-k,0)
 
-class Parametri2d{
+class Parametri{
 public:
 	//Dati
 	double T;                                               // Scadenza
 	double K;                                               // Strike price
-	double S01;                                             // Spot price
-	double S02;                                             // Spot price
+	double S0;                                              // Spot price
 	double r;                                               // Tasso risk free
         
 	// Parametri della parte continua
-	double sigma1;                                          // Volatilità
-        double sigma2;                                          // Volatilità
-        double ro;                                              // Volatilità
+	double sigma;                                           // Volatilità
         
         // Parametri della parte salto
         double p;                                               // Parametro 1 Kou
@@ -77,21 +76,58 @@ public:
         double lambda_piu;                                      // Parametro 3 Kou
         double lambda_meno;                                     // Parametro 4 Kou
         
-	Parametri2d()=default;
-	Parametri2d(const Parametri2d &)=default;
+	Parametri()=default;
+	Parametri(const Parametri &)=default;
 };
+
+template <int dim>
+class Coefficient : public Function<dim>
+{
+public:
+        Coefficient (double _r, double _S0)  : Function<dim>(), r(_r), S0(_S0) {}
+        virtual double value (const Point<dim>   &p,
+                              const unsigned int  component = 0) const;
+        virtual void value_list (const std::vector<Point<dim> > &points,
+                                 std::vector<double>            &values,
+                                 const unsigned int              component = 0) const;
+private:
+        double r;
+        double S0;
+};
+
+template <int dim>
+double Coefficient<dim>::value (const Point<dim> &p,
+                                const unsigned int /*component*/) const
+{
+        return r*S0*exp(p(0));
+}
+
+template <int dim>
+void Coefficient<dim>::value_list (const std::vector<Point<dim> > &points,
+                                   std::vector<double>            &values,
+                                   const unsigned int              component) const
+{
+        Assert (values.size() == points.size(),
+                ExcDimensionMismatch (values.size(), points.size()));
+        Assert (component == 0,
+                ExcIndexRange (component, 0, 1));
+        const unsigned int n_points = points.size();
+        for (unsigned int i=0; i<n_points; ++i)
+        {
+                values[i]=r*S0*exp(points[i][0]);
+        }
+}
 
 template<int dim>
 class Boundary_Condition: public Function<dim>
 {
 public:
-	Boundary_Condition(double S01, double S02, double K, double T,  double r) : Function< dim>(),
-        _S01(S01), _S02(S02), _K(K), _T(T), _r(r) {};
+	Boundary_Condition(double S0, double K, double T,  double r) : Function< dim>(),
+        _S0(S0), _K(K), _T(T), _r(r) {};
         
 	virtual double value (const Point<dim> &p, const unsigned int component =0) const;
 private:
-        double _S01;
-        double _S02;
+        double _S0;
 	double _K;
 	double _T;
 	double _r;
@@ -101,7 +137,7 @@ template<int dim>
 double Boundary_Condition<dim>::value(const Point<dim> &p, const unsigned int component) const
 {
 	Assert (component == 0, ExcInternalError());
-        return max(_S01*exp(p[0])+_S02*exp(p[1])-_K*exp(-_r*(_T-this->get_time())),0.);
+        return max(_S0*exp(p[0])-p[1],0.);
         
 }
 
@@ -109,14 +145,13 @@ template<int dim>
 class PayOff : public Function<dim>
 {
 public:
-        PayOff (double K_, double S01_, double S02_) : Function<dim>(), K(K_), S01(S01_), S02(S02_) {};
+        PayOff (double K_, double S0_) : Function<dim>(), K(K_), S0(S0_) {};
         
         virtual double value (const Point<dim>   &p,
                               const unsigned int  component = 0) const;
 private:
         double K;
-        double S01;
-        double S02;
+        double S0;
 };
 
 template<int dim>
@@ -124,13 +159,13 @@ double PayOff<dim>::value (const Point<dim>  &p,
                            const unsigned int component) const
 {
         Assert (component == 0, ExcInternalError());
-        return max(S01*exp(p(0))+S02*exp(p(1))-K,0.);
+        return max(S0*exp(p[0])-p[1],0.);
 }
 
 template<int dim>
 class Opzione{
 private:
-	Parametri2d par;
+	Parametri par;
 	void make_grid();
 	void setup_system () ;
 	void assemble_system () ;
@@ -157,13 +192,14 @@ private:
         
         unsigned int refs, Nsteps;
 	double time_step;
-        double dx1, dx2;
-	double Smin1, Smax1, Smin2, Smax2;
-        double xmin1, xmax1, xmin2, xmax2;
+        double dx;
+	double Smin, Smax;
+        double xmin, xmax;
+        double Amin, Amax;
         
         bool ran;
 public:
-        Opzione(Parametri2d const &par_, int Nsteps_,  int refinement):
+        Opzione(Parametri const &par_, int Nsteps_,  int refinement):
 	par(par_),
 	fe (1),
 	dof_handler (triangulation),
@@ -189,32 +225,32 @@ public:
 template<int dim>
 void Opzione<dim>::make_grid() {
 	
-        Smin1=par.S01*exp((par.r-par.sigma1*par.sigma1/2)*par.T
-                            -par.sigma1*sqrt(par.T)*6);
-	Smax1=par.S01*exp((par.r-par.sigma1*par.sigma1/2)*par.T
-                            +par.sigma1*sqrt(par.T)*6);
-        Smin2=par.S02*exp((par.r-par.sigma2*par.sigma2/2)*par.T
-                         -par.sigma2*sqrt(par.T)*6);
-	Smax2=par.S02*exp((par.r-par.sigma2*par.sigma2/2)*par.T
-                         +par.sigma2*sqrt(par.T)*6);
+        Smin=par.S0*exp((par.r-par.sigma*par.sigma/2)*par.T
+                            -par.sigma*sqrt(par.T)*6);
+	Smax=par.S0*exp((par.r-par.sigma*par.sigma/2)*par.T
+                            +par.sigma*sqrt(par.T)*6);
         
-	xmin1=log(Smin1/par.S01);
-        xmax1=log(Smax1/par.S01);
-        xmin2=log(Smin2/par.S02);
-        xmax2=log(Smax2/par.S02);
+	cout<< "Smin= "<< Smin<< "\t e Smax= "<< Smax<< endl;
+	xmin=0;
+        xmax=0;
         
-        cout<< "Smin1= "<< Smin1<< "\t e Smax1= "<< Smax1<< endl;
-	cout<< "Smin2= "<< Smin2<< "\t e Smax2= "<< Smax2<< endl;
-	cout<< "xmin1= "<< xmin1<< "\t e xmax1= "<< xmax1<< endl;
-	cout<< "xmin2= "<< xmin2<< "\t e xmax2= "<< xmax2<< endl;
+        dx=(log(Smax/par.S0)-log(Smin/par.S0))/pow(2., refs);
+        cout<<"dx "<<dx<<"\n";
         
-        dx1=(xmax1-xmin1)/pow(2., refs);
-        dx2=(xmax2-xmin2)/pow(2., refs);
+        while (xmin>log(Smin/par.S0))
+                xmin-=dx;
+        while (xmax<log(Smax/par.S0))
+                xmax+=dx;
+        xmin-=dx;
+        xmax+=dx;
         
-        Point<dim> p1(xmin1,xmin2);
-        Point<dim> p2(xmax1,xmax2);
+        Amin=0;
+        Amax=Smax*par.T;
         
-        std::vector<unsigned> refinement={pow(2,refs), pow(2,refs)};
+        Point<dim> p1(xmin,Amin);
+        Point<dim> p2(xmax,Amax);
+        
+        std::vector<unsigned> refinement={pow(2,refs)+3, pow(2,refs)};
         
         GridGenerator::subdivided_hyper_rectangle(triangulation, refinement, p1, p2);
         
@@ -254,8 +290,8 @@ void Opzione<dim>::assemble_system() {
         
         QGauss<dim> quadrature_formula(4); // 2 nodes, 2d -> 4 quadrature points per cell
         
-	FEValues<dim> fe_values (fe, quadrature_formula, update_values   | update_gradients |
-                                 update_JxW_values);
+	FEValues<dim> fe_values (fe, quadrature_formula, update_values   | update_gradients | update_quadrature_points |
+                                 update_JxW_values); //  update_quadrature_points ?
         
 	const unsigned int   dofs_per_cell = fe.dofs_per_cell;
 	const unsigned int   n_q_points    = quadrature_formula.size();
@@ -278,29 +314,42 @@ void Opzione<dim>::assemble_system() {
         // building tensors
         Tensor< dim , dim, double > sigma_matrix;
         
-        sigma_matrix[0][0]=par.sigma1;
-        sigma_matrix[1][1]=par.sigma2;
-        sigma_matrix[0][1]=par.sigma1*par.sigma2*par.ro;
-        sigma_matrix[1][0]=par.sigma1*par.sigma2*par.ro;
+        sigma_matrix[0][0]=par.sigma;
+        sigma_matrix[1][1]=0.;
+        sigma_matrix[0][1]=0.;
+        sigma_matrix[1][0]=0.;
         
         Tensor< 1 , dim, double > ones;
-	for (unsigned i=0;i<dim;++i)
+	
+        for (unsigned i=0;i<dim;++i)
                 ones[i]=1;
         
         Tensor< 1, dim, double > trasp;
-        trasp[0]=par.r-par.sigma1*par.sigma1/2;
-        trasp[1]=par.r-par.sigma2*par.sigma2/2;
+        
+        trasp[0]=par.r-par.sigma*par.sigma/2;
+        trasp[1]=0.;
+        
+        const Coefficient<dim> coefficient(par.r, par.S0);
+        std::vector<double>    coefficient_values (n_q_points);
         
         // cell loop
         for (; cell !=endc;++cell) {
+                
                 fe_values.reinit(cell);
+                
                 cell_dd=0;
                 cell_fd=0;
                 cell_ff=0;
                 cell_system=0;
+                
+                coefficient.value_list (fe_values.get_quadrature_points(),
+                                        coefficient_values);
+                
                 for (unsigned q_point=0;q_point<n_q_points;++q_point)
                         for (unsigned i=0;i<dofs_per_cell;++i)
                                 for (unsigned j=0; j<dofs_per_cell;++j) {
+                                        
+                                        trasp[1]=coefficient_values[q_point];
                                         
                                         cell_dd(i, j)+=fe_values.shape_grad(i, q_point)*sigma_matrix*fe_values.shape_grad(j, q_point)*fe_values.JxW(q_point);
                                         cell_fd(i, j)+=fe_values.shape_value(i, q_point)*(ones*fe_values.shape_grad(j,q_point))*fe_values.JxW(q_point);
@@ -347,7 +396,7 @@ void Opzione<dim>::assemble_system() {
 template<int dim>
 void Opzione<dim>::solve() {
 	
-	VectorTools::interpolate (dof_handler, PayOff<dim>(par.K, par.S01, par.S02), solution);
+	VectorTools::interpolate (dof_handler, PayOff<dim>(par.K, par.S0), solution);
 	
 	unsigned int Step=Nsteps;
         
@@ -365,7 +414,7 @@ void Opzione<dim>::solve() {
         }
         //
         
-        Boundary_Condition<dim> bc(par.S01, par.S02, par.K, par.T, par.r);
+        Boundary_Condition<dim> bc(par.S0, par.K, par.T, par.r);
 	cout<< "time step is"<< time_step<< endl;
 	for (double time=par.T-time_step;time >=0;time-=time_step, --Step) {
                 cout<< "Step "<< Step<<"\t at time \t"<< time<< endl;
@@ -436,15 +485,12 @@ void Opzione<dim>::solve() {
 }
 
 int main() {
-	Parametri2d par;
-	par.T=1.;
-	par.K=200;
-	par.S01=100;
-        par.S02=100;
+	Parametri par;
+	par.T=5.;
+	par.K=100;
+	par.S0=100;
 	par.r=0.0367;
-	par.sigma1=0.120381;
-        par.sigma2=0.09;
-        par.ro=0.2;
+	par.sigma=0.120381;
         
         // Parametri della parte salto
         par.p=0.20761;           // Parametro 1 Kou
@@ -453,7 +499,7 @@ int main() {
         par.lambda_meno=3.13868; // Parametro 4 Kou
         
         // tempo // spazio
-	Opzione<2> Call(par, 100, 6);
+	Opzione<2> Call(par, 100, 5);
 	double prezzo=Call.run();
         
         cout<<"Prezzo "<<prezzo<<"\n";
