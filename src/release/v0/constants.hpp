45cc4cf068b1cde0db9f23deede84626adc35a2a
baba12345 <giorgio.re27@gmail.com>
2014-05-26 20:59:13 +0200

    StartingOrganizingNotWorking

---
+++ src/release/v0/constants.hpp
@@ -0,0 +1,7 @@
+#include <climits>
+
+namespace constants {
+        const double light_toll=1.e-8;
+        const double high_toll=1.e-12;
+        const double eps=std::numeric_limits<double>::epsilon();
+}
