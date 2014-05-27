399b202d25160b50eba2e577e5c7a22d8f8d71cc
baba12345 <giorgio.re27@gmail.com>
2014-05-27 20:54:02 +0200

    JustPlaying

--- src/release/v0/test.cpp
+++ src/release/v0/test.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 int main(){
         
-        Model<ModelType::BlackScholes> model0(95., 0.120381);
-        
         BlackScholesModel model(95., 0.120381);
         
-        Option<1, ModelType::BlackScholes> foo(model, 0.0367, 1., 90., 10, 100);
+        Option<1> foo(model, 0.0367, 1., 90., 10, 100);
         
         foo.run();
         
