0074f76035e5f9a0ef4417c0e72271c512605b5b
NTFr <nahuel.foresta@gmail.com>
2014-06-10 14:14:22 +0200

    modified variables names

--- src/2d/European_2d/VertexIterator/main.cpp
+++ src/2d/European_2d/VertexIterator/main.cpp
@@ -221,15 +221,15 @@ class Vertex_Iterator {
 private:
 	typename dealii::DoFHandler<dim>::active_cell_iterator _cells;
 	std::vector<bool> _used;
-	unsigned _j;
-	unsigned _nd;
+	unsigned _vert_index;
+	unsigned _ndofs_cell;
 	unsigned _counted;
 
 public:
 	Vertex_Iterator()=delete;
-	Vertex_Iterator(dealii::DoFHandler<dim> const & dof): _used(dof.n_dofs(), false), _j(0),  _counted(1) {
+	Vertex_Iterator(dealii::DoFHandler<dim> const & dof): _used(dof.n_dofs(), false), _vert_index(0),  _counted(1) {
 
-	 _nd=dof.get_fe().dofs_per_cell;
+	 _ndofs_cell=dof.get_fe().dofs_per_cell;
 	 _cells=dof.begin_active();
    }
 
@@ -241,16 +241,16 @@ public:
 	bool operator== (Vertex_Iterator<dim> const & rhs);
 	bool operator!= (Vertex_Iterator<dim> const & rhs);
 	Vertex_Iterator & operator++ ();
-	Point<dim> & operator* () { return _cells->vertex(_j);}
+	Point<dim> & operator* () { return _cells->vertex(_vert_index);}
 
   };
 
 
 template<int dim>
 dealii::types::global_dof_index Vertex_Iterator<dim>::get_global_index() {
-	std::vector<dealii::types::global_dof_index> local_ind(_nd);
+	std::vector<dealii::types::global_dof_index> local_ind(_ndofs_cell);
 	_cells->get_dof_indices(local_ind);
-	return local_ind[_j];
+	return local_ind[_vert_index];
   }
 
 template<int dim>
@@ -258,11 +258,11 @@ Vertex_Iterator<dim> & Vertex_Iterator<dim>::operator++() {
 	if (_counted<_used.size())
 	{
 	 do{
-	  if (_j<_nd-1)
-	  _j++;
+	  if (_vert_index<_ndofs_cell-1)
+	  _vert_index++;
 	  else {
 	   ++_cells;
-	   _j=0;
+	   _vert_index=0;
 	 }
 
 	} while (_used[get_global_index()]);
@@ -276,13 +276,13 @@ Vertex_Iterator<dim> & Vertex_Iterator<dim>::operator++() {
 
 template<int dim>
 bool Vertex_Iterator<dim>::operator== (Vertex_Iterator<dim> const & rhs) {
-	return (_cells==rhs._cells && _j==rhs._j);
+	return (_cells==rhs._cells && _vert_index==rhs._vert_index);
   }
 
 
 template<int dim>
 bool Vertex_Iterator<dim>::operator != (Vertex_Iterator<dim> const & rhs) {
-	return (_cells !=rhs._cells or _j !=rhs._j);
+	return (_cells !=rhs._cells or _vert_index !=rhs._vert_index);
   }
 
 template<int dim>
