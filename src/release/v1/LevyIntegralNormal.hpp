5fb80909469c5086b458700d814d02a604d0574b
NTFr <nahuel.foresta@gmail.com>
2014-07-19 23:04:26 +0200

    base class done

--- src/release/v1/LevyIntegralNormal.hpp
+++ src/release/v1/LevyIntegralNormal.hpp
@@ -5,39 +5,100 @@
 # include "Quadrature.hpp"
 # include "Densities.hpp"
 # include "tools.hpp"
+#include "models.hpp"
+
+//TODO
+//constructor
+//inherit rest 
 
 template<unsigned dim>
 class LevyIntegral<dim> {
 
-private:
+protected:
 	std::vector<double> alpha;
 	dealii::Vector<double> J1, J2;
+	dealii::Point<dim> Bmin, Bmax,  Smax;
 	
-	std::vector<>
+	std::vector<Model *> Mods;
 	
 	bool alpha_ran;
 	
-	void compute_alpha();
-	
-	dealii::Vector<double> J1, J2;
+	virtual void compute_alpha();
+	virtual void compute_J(/* */) =0;
 
 public:
-	double get_alpha_1() {if (!alpha_ran)
-							compute_alpha();
-							else return alpha[0];}
 	
-	double get_alpha_2() {if (!alpha_ran)                      // add exception if dim < 2
+	LevyIntegral()=delete;
+	
+	virtual double get_alpha_1() const {if (!alpha_ran)
+							compute_alpha();
+							return alpha[0];}
+	
+	virtual double get_alpha_2() const {if (!alpha_ran)                      // add exception if dim < 2
 							 compute_alpha();
-							 else return alpha[1];}
+							 return alpha[1];}
+
+	virtual void get_alpha(std::vector<double> & alp) const {if (!alpha_ran)
+														  compute_alpha();
+															alp=alpha;
+														 return;}
+
+	virtual ~LevyIntegral() {for (unsiged d=0;d<dim;++d) Mods[d]=nullptr;}
 };
 
 template<unsigned dim>
 void LevyIntegral< dim >::compute_alpha()
 {
 for (unsigned d=0;d<dim;++d) {
+	Bmin[d]=0.;Bmax[d]=Smax[d];
+	while ((*Mods[d]).density(Bmin[d])>tol)
+		Bmin[d]+=-0.5;
+	while ((*Mods[d]).density(Bmax[d])>tol)
+		Bmax[d]+=0.5;
+	
+	
+	 Triangulation<1> integral_grid;
+	 FE_Q<1> fe2(1);
+	 DoFHandler<1> dof_handler2(integral_grid);
 
+	 GridGenerator::hyper_cube<1>(integral_grid, Bmin[d], Bmax[d]);
+	 integral_grid.refine_global(15);
+
+	 dof_handler2.distribute_dofs(fe2);
+
+	 QGauss<1> quadrature_formula(8);
+	 FEValues<1> fe_values(fe2, quadrature_formula,  update_quadrature_points |update_JxW_values);
+
+	 typename DoFHandler<1>::active_cell_iterator
+	 cell=dof_handler2.begin_active(),
+	 endc=dof_handler2.end();
+
+	 const unsigned int n_q_points(quadrature_formula.size());
+
+	 for (; cell !=endc;++cell) {
+	  fe_values.reinit(cell);
+	  std::vector< Point<1> > quad_points_1D(fe_values.get_quadrature_points());
+
+	  for (unsigned q_point=0;q_point<n_q_points;++q_point) {
+	   Point<dim> p;
+	   p[d]=quad_points_1D[q_point][d];
+	   alpha[d]+=fe_values.JxW(q_point)*(exp(p[d])-1.)*(*Mods[d]).density(p[d]);
+	 }
+	}
+
+	 std::cout<< "alpha "<< d << " is "<< alpha[d]<< std::endl;
+ }
+	
 }
-}
+
+template<unsigned dim>
+class LevyIntegralPrice: public: LevyIntegral< dim > {
+
+protected:
+	virtual void compute J
+	
+};
+
 
 
 #endif
