2368b7328cfa6c6985ec919b0b2e20fec2493fa2
NTFr <nahuel.foresta@gmail.com>
2014-07-28 15:53:51 +0200

    added gridtol const

--- src/release/v1/constants.hpp
+++ src/release/v1/constants.hpp
@@ -6,6 +6,7 @@
 //! We gather here some of the constants used in the whole program.
 namespace constants {
         const double pi=4.*std::atan(1.);
+        const double grid_toll=1.e-5;
         const double light_toll=1.e-8;
         const double high_toll=1.e-12;
         const double eps=std::numeric_limits<double>::epsilon();
