fbf06a0699ea80348568c02c2a12bb9bfa2d5fa3
baba12345 <giorgio.re27@gmail.com>
2014-07-29 21:47:47 +0200

    KouIsMean

--- src/release/v1/LevyIntegralLogPriceKou.hpp
+++ src/release/v1/LevyIntegralLogPriceKou.hpp
@@ -130,6 +130,74 @@ void LevyIntegralLogPriceKou<dim>::compute_J(dealii::Vector< double >& sol, deal
                         
                         for (unsigned i=0; i<leftQuads[d].get_order(); ++i) {
                                 quad_points[i][d]=this->leftQuads[d].get_nodes()[i] + vertices[it][d];
+                                if (dim==2) {
+                                        quad_points[i][1-d]=vertices[it][1-d];
+                                }
+                        }
+                        
+                        for (unsigned i=0; i<rightQuads[d].get_order(); ++i) {
+                                quad_points[i+leftQuads[d].get_order()][d]=
+                                this->rightQuads[d].get_nodes()[i] + vertices[it][d];
+                                if (dim==2) {
+                                        quad_points[i+leftQuads[d].get_order()][1-d]=vertices[it][1-d];
+                                }
+                        }
+                        
+                        // valuto f_u in quad_points
+                        func.value_list(quad_points, f_u);
+                        
+                        // Integro dividendo fra parte sinistra e parte destra dell'integrale
+                        for (unsigned i=0;i<leftQuads[d].get_order();++i) {
+                                J[d*N+it]+=f_u[i]*(1-((this->Mods[d])->get_p()))*((this->Mods[d])->get_lambda())*
+                                ((this->Mods[d])->get_lambda_m())*(leftQuads[d].get_weights())[i];
+                        }
+                        
+                        for (unsigned i=0;i<rightQuads[d].get_order();++i) {
+                                J[d*N+it]+=f_u[i+leftQuads[d].get_order()]*((this->Mods[d])->get_p())*((this->Mods[d])->get_lambda())*
+                                ((this->Mods[d])->get_lambda_p())*(rightQuads[d].get_weights())[i];
+                        }
+                        
+                }
+                
+        }
+	this->J1.reinit(N);
+	for (unsigned i=0;i<this->J1.size();++i)
+                this->J1[i]=J[i];
+	if (dim==2) {
+                this->J2.reinit(N);
+                for (unsigned i=0;i<this->J1.size();++i)
+                        this->J2[i]=J[i+N];
+        }
+        
+        //std::cout<<"***J***\n"<<this->J1<<"\n";
+        
+        this->j_ran=true;
+}
+/*
+template<unsigned dim>
+void LevyIntegralLogPriceKou<dim>::compute_J(dealii::Vector< double >& sol, dealii::DoFHandler<dim>& dof_handler, dealii::FE_Q<dim>& fe)
+{
+        
+        //std::cout<<"calcolo J\n";
+	using namespace dealii;
+	unsigned N(sol.size());
+	//TODO and if we do not initialise J nd do a pushback? 
+	Vector<double> J;
+        J.reinit(2*N);
+	std::map<types::global_dof_index, Point<dim> > vertices;
+	DoFTools::map_dofs_to_support_points(MappingQ1<dim>(), dof_handler, vertices);
+        
+	for (unsigned d=0;d<dim;++d) {
+                tools::Solution_Trimmer<dim> func(d,*(this->boundary), dof_handler, sol, this->lower_limit, this->lower_limit);
+                
+                //#pragma omp parallel for
+                for (unsigned int it=0;it<N;++it)
+                {
+                        std::vector< Point<dim> > quad_points(leftQuads[d].get_order()+rightQuads[d].get_order());
+                        std::vector<double> f_u(leftQuads[d].get_order()+rightQuads[d].get_order());
+                        
+                        for (unsigned i=0; i<leftQuads[d].get_order(); ++i) {
+                                quad_points[i][d]=this->leftQuads[d].get_nodes()[i] + vertices[it][d];
                                 quad_points[i][1-d]=vertices[it][1-d];
                         }
                         
@@ -168,5 +236,6 @@ void LevyIntegralLogPriceKou<dim>::compute_J(dealii::Vector< double >& sol, deal
         
         this->j_ran=true;
 }
+*/
 
 #endif
