febfbb80ad93af04e352e6cdb1cdd87cbec8d419
NTFr <nahuel.foresta@gmail.com>
2014-08-04 17:55:53 +0200

    corrected time in bc

--- src/release/v1/EuropeanOptionPrice.hpp
+++ src/release/v1/EuropeanOptionPrice.hpp
@@ -142,7 +142,7 @@ void EuropeanOptionPrice<dim>::solve ()
                 
                 //
                 
-                bc.set_time(this->dt);
+                bc.set_time(time);
                 
                 {
                         
