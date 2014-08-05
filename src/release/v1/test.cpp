6f3ae5a171cc39becec259aa1a65b7096866447d
NTFr <nahuel.foresta@gmail.com>
2014-08-05 16:23:47 +0200

    state of art updated

--- src/release/v1/test.cpp
+++ src/release/v1/test.cpp
@@ -28,7 +28,7 @@ int main(){
         
         EuropeanOptionPrice<2> c
         (OptionType::Call, model4.get_pointer(), model5.get_pointer(),
-         -0.2, 0.1, 1., 200., 6, 100);
+         -0.2, 0.1, 1., 200., 7, 100);
         
         EuropeanOptionLogPrice<2> d
         (OptionType::Call, model4.get_pointer(), model5.get_pointer(),
@@ -36,18 +36,18 @@ int main(){
 		
         //a.set_refine_status(true);
  	//b.set_refine_status(true);
-        //c.set_refine_status(true, 0.1);		
+        c.set_refine_status(true, 0.1);		
 //         d.set_refine_status(true);
 		
-        a.run();
-        b.run();
-//        c.run();
+//         a.run();
+//         b.run();
+       c.run();
 //        d.run();
 // 		d.print_grid("Griglia");
 // 		d.print_solution_gnuplot("Soluzione");
-        cout<<a.get_price()<<"\n";
-        cout<<b.get_price()<<"\n";
-//        cout<<c.get_price()<<"\n";
+//         cout<<a.get_price()<<"\n";
+//         cout<<b.get_price()<<"\n";
+       cout<<c.get_price()<<"\n";
 //        cout<<d.get_price()<<"\n";
         
 	/*
