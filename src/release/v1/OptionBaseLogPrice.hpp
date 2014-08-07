a5b7872de48dbb8ad32887f01de40a476e483c8e
NTFr <nahuel.foresta@gmail.com>
2014-08-07 19:49:26 +0200

    A nicer assembly, will it work? Seems so

--- src/release/v1/OptionBaseLogPrice.hpp
+++ src/release/v1/OptionBaseLogPrice.hpp
@@ -3,6 +3,7 @@
 
 #include "OptionBase.hpp"
 #include "BoundaryConditionsLogPrice.hpp"
+#include "tools.hpp"
 
 template <unsigned dim>
 class OptionBaseLogPrice: public OptionBase<dim> {
@@ -97,7 +98,7 @@ void OptionBaseLogPrice<dim>::assemble_system()
                         lambda+=this->models[i]->get_lambda();
         
         dealii::QGauss<dim> quadrature_formula(2*dim);
-	dealii::FEValues<dim> fe_values (this->fe, quadrature_formula, update_values |
+		dealii::FEValues<dim> fe_values (this->fe, quadrature_formula, update_values |
                                          update_gradients |
                                          update_JxW_values | update_quadrature_points);
         
@@ -124,12 +125,64 @@ void OptionBaseLogPrice<dim>::assemble_system()
 	typename dealii::DoFHandler<dim>::active_cell_iterator
 	cell=(this->dof_handler).begin_active(),
 	endc=(this->dof_handler).end();
-	Tensor< 1 , dim, double > trasp;
-	Tensor< 2 , dim, double > sig_mat;
+	
+
 	
         std::vector<Point<dim> > quad_points(n_q_points);
         
         std::cout<<alpha[0]<<"\n";
+        
+        dealii::Tensor<1, dim, double> trasp;
+		Tensor<2, dim, double> sigma_matrix;
+
+		Point<dim> dummy_point;
+		
+		for (unsigned d=0;d<dim;++d) {
+			trasp[d]=this->r-(*(this->models[d])).get_vol()*(*(this->models[d])).get_vol()/2-alpha[d];
+			dummy_point(d)=1.;
+		}
+		
+		tools::make_diff<dim>(sigma_matrix, this->models, this->rho, dummy_point);
+        
+		for (; cell !=endc;++cell) {
+			fe_values.reinit(cell);
+			cell_ff=0;
+			cell_system=0;
+			
+			for (unsigned q_point=0;q_point<n_q_points;++q_point)
+				for (unsigned i=0;i<dofs_per_cell;++i)
+				  for (unsigned j=0; j<dofs_per_cell;++j) {
+
+					   // mass matrix
+					   cell_ff(i, j)+=fe_values.shape_value(i, q_point)*fe_values.shape_value(j, q_point)*fe_values.JxW(q_point);
+
+					   // system matrix
+					   cell_system(i, j)+=fe_values.JxW(q_point)*
+					   (fe_values.shape_grad(i, q_point)*sigma_matrix*fe_values.shape_grad(j, q_point)-
+						fe_values.shape_value(i, q_point)*(trasp*fe_values.shape_grad(j,q_point))+
+						(1/(this->dt)+this->r+lambda)*
+						fe_values.shape_value(i, q_point)*fe_values.shape_value(j, q_point));
+
+			 }
+		
+	  cell->get_dof_indices (local_dof_indices);
+
+	  for (unsigned int i=0; i<dofs_per_cell;++i)
+		  for (unsigned int j=0; j< dofs_per_cell; ++j) {
+
+			   (this->ff_matrix).add(local_dof_indices[i], local_dof_indices[j], cell_ff(i, j));
+			   ((this->system_matrix)).add(local_dof_indices[i], local_dof_indices[j], cell_system(i, j));
+
+	 }
+		
+	  	 
+}
+	(this->system_M2).add(1/(this->dt), this->ff_matrix);
+        
+        
+        /*
+		Tensor< 1 , dim, double > trasp;
+		Tensor< 2 , dim, double > sig_mat;
         
         if (dim==1) {
                 dealii::Tensor< 1 , dim, double > ones;
@@ -168,14 +221,21 @@ void OptionBaseLogPrice<dim>::assemble_system()
                 double trasp=this->r-(*(this->models[0])).get_vol()*
                 (*(this->models[0])).get_vol()/2-alpha[0];
                 double reaz=-(this->r)-lambda;
+
+//                 ((this->system_matrix)).add(1/(this->dt)-0.5*reaz, this->ff_matrix); 
+//                 ((this->system_matrix)).add(0.5*diff, dd_matrix);
+//                 ((this->system_matrix)).add(-0.5*trasp, fd_matrix);
+//                 
+//                 (this->system_M2).add(1/(this->dt)+0.5*reaz, this->ff_matrix); 
+//                 (this->system_M2).add(-0.5*diff, dd_matrix);
+//                 (this->system_M2).add(0.5*trasp, fd_matrix);
                 
-                ((this->system_matrix)).add(1/(this->dt)-0.5*reaz, this->ff_matrix); 
-                ((this->system_matrix)).add(0.5*diff, dd_matrix);
-                ((this->system_matrix)).add(-0.5*trasp, fd_matrix);
-                
-                (this->system_M2).add(1/(this->dt)+0.5*reaz, this->ff_matrix); 
-                (this->system_M2).add(-0.5*diff, dd_matrix);
-                (this->system_M2).add(0.5*trasp, fd_matrix);
+			((this->system_matrix)).add(1/(this->dt)-reaz, this->ff_matrix); 
+			((this->system_matrix)).add(diff, dd_matrix);
+			((this->system_matrix)).add(-trasp, fd_matrix);
+
+			(this->system_M2).add(1/(this->dt), this->ff_matrix); 
+
                 
         }
         
@@ -189,11 +249,11 @@ void OptionBaseLogPrice<dim>::assemble_system()
                 (this->rho);
                 sigma_matrix[1][0]=(*(this->models[0])).get_vol()*(*(this->models[1])).get_vol()*
                 (this->rho);
-                /*
-                 Tensor< 1 , dim, double > ones;
-                 for (unsigned i=0;i<dim;++i)
-                 ones[i]=1;
-                 */
+                
+//                  Tensor< 1 , dim, double > ones;
+//                  for (unsigned i=0;i<dim;++i)
+//                  ones[i]=1;
+                 
                 Tensor< 1, dim, double > trasp;
                 trasp[0]=this->r-(*(this->models[0])).get_vol()*(*(this->models[0])).get_vol()/2-alpha[0];
                 trasp[1]=this->r-(*(this->models[1])).get_vol()*(*(this->models[1])).get_vol()/2-alpha[1];
@@ -234,7 +294,7 @@ void OptionBaseLogPrice<dim>::assemble_system()
                 
                 (this->system_M2).add(1/(this->dt), this->ff_matrix);
         }
-        
+        */
         return;
         
 }
