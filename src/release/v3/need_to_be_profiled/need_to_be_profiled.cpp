2481153beb09142c1304149af9919acf9b36a14a
baba12345 <giorgio.re27@gmail.com>
2014-08-16 16:42:36 +0200

    NeedToProfile

---
+++ src/release/v3/need_to_be_profiled/need_to_be_profiled.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Factory.hpp"
+
+int main(){
+	
+        using namespace dealii;
+        using namespace std;
+        
+        KouModel model(95, 0.120381, 0.20761, 0.330966, 9.65997, 3.13868);
+        
+        omp_set_num_threads(1);
+        
+        auto foo=Factory::instance()->create(ExerciseType::EU,
+                                                     OptionType::Call,
+                                                     Transformation::LogPrice,
+                                                     model.get_pointer(),
+                                                     0.0367, 1., 90., 8, 100);
+                
+        foo->set_verbose(false);
+        
+        foo->run();
+        
+        return 0;
+        
+}
