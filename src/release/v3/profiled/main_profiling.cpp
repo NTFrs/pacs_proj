5ce61e6f502d0087afc137230f3b542fc7b9d916
baba12345 <giorgio.re27@gmail.com>
2014-08-22 14:01:49 +0200

    NewProfile

--- src/release/v3/profiled/main_profiling.cpp
+++ src/release/v3/profiled/main_profiling.cpp
@@ -8,7 +8,7 @@ int main(){
         
         KouModel model(95, 0.120381, 0.20761, 0.330966, 9.65997, 3.13868);
         
-//         omp_set_num_threads(1);
+        omp_set_num_threads(1);
         
         auto foo=Factory::instance()->create(ExerciseType::EU,
                                                      OptionType::Call,
@@ -17,7 +17,8 @@ int main(){
                                                      0.0367, 1., 90., 8, 100);
                 
         foo->set_verbose(false);
-        
+        foo->set_integral_adaptivity_params(false, 16);
+
         foo->run();
         
         return 0;
